b49d6d73c624e57a0f7bc7028a16e2a9896ad3df
gyanAsh <gyanash36@protonmail.com>
2022-09-04 02:47:13 +0530

    Header created

---
+++ components/Wrapper/Header.js
@@ -0,0 +1,20 @@
+import Image from 'next/image'
+import React from 'react'
+import myLogo  from '../../public/headerLogo.svg'
+
+const Header = () => {
+  return (
+      <div className="h-24 bg-slate-900 text-white flex items-center justify-between px-8">
+          <Image src={myLogo} alt="Overlay Icon" />
+          <Button/>
+    </div>
+  )
+}
+
+export default Header
+
+const Button = () => {
+    return (
+        <button className="w-36 h-14 bg-blue-700 rounded-xl ">Sign In</button>
+    )
+}
