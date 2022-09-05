e4a914405ac6606f38290614fc3d8fba395cb107
gyanAsh <gyanash36@protonmail.com>
2022-09-05 17:15:28 +0530

    SignIn css for large screen compmleted

--- components/Auth/SignIn.js
+++ components/Auth/SignIn.js
@@ -1,12 +1,13 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Image from 'next/image'
 import style from '../../styles/Image.module.css'
 import illustration from '../../public/SignIn/illustration.png'
+import googleIcon from '../../public/google.svg'
 import logo from '../../public/Logo.svg'
 import Footer from '../Wrapper/Footer'
 const SignIn = () => {
   return (
-    <div className="bg-white flex justify-between h-screen">
+    <div className="bg-white flex justify-between h-screen font-rope">
       <Login/>
       <Image className={style.override} objectFit="contain" src={illustration} alt="Get better with money" />
     </div>
@@ -26,27 +27,38 @@ const Login = () => {
 }
 
 const Form = () => {
+  const [viewPassword, setViewPassword] = useState(false)
+  const [remember,setRemember] = useState(false)
   return (
-    <div className="h-full flex flex-col justify-center items-center">
-      <div>
-        <h1>Sign in to Overpay</h1>
-        <h5>Send, spend and save smater</h5>
-      </div>
-      <button>Sign in with Google</button>
+    <div className="flex flex-col justify-center items-center h-5/6">
+      <div className="w-96">
+        <div className="flex flex-col items-center font-rope gap-4 mb-10">
+          <h3 className="text-gray-900 text-center text-3xl font-bold">Sign in to Overpay</h3>
+          <h5 className="text-center text-base font-normal text-slate-500">Send, spend and save smater</h5>
+        </div>
+        <button className="border border-zinc-300 rounded-xl h-14 w-full flex justify-center items-center"><Image objectFit="contain" width={24} src={googleIcon} alt="Google Icon"/>Sign in with Google</button>
+        <div className="flex flex-row items-center flex-rope mt-4 mb-4">
+          <span className="border-t border-zinc-300 w-full"></span>
+          <p className="w-full text-center text-base font-normal text-slate-500">Or With Email</p>
+          <span className="border-t border-zinc-300 w-full"></span>
+        </div>
       <form className="flex flex-col">
-        <div className="flex flex-col relative">
-          <input className="border border-zinc-300 rounded-xl" type="email" />
-          <input className="border border-zinc-300 rounded-xl" type="password" />
-          <button className="absolute bottom-0 right-0">eye close</button>
+        <div className="flex flex-col relative gap-4">
+          <input className="border border-zinc-300 rounded-xl h-14 w-full p-4 text-base font-rope font-medium tracking-wider" type="email" placeholder='Username or email'/>
+            <input className="border border-zinc-300 rounded-xl h-14 w-full p-4 text-base font-rope font-medium tracking-wider" type={viewPassword?"text":"password"} placeholder='Password'/>
+            <button className="absolute bottom-4 right-4" onClick={(e) => { e.preventDefault(); setViewPassword(!viewPassword) }}><i className={`fa-regular fa-${viewPassword?'eye':'eye-slash'}`}></i></button>
         </div>
-        <div>
-          <input type="radio" id="remember" name="remember" value="yes"/>
-          <label for="remember">remember me</label>
-          <h5>Forgot Password?</h5>
+          <div className="flex justify-between mt-6">
+              <button for="remember" className="text-base font-semibold text-slate-800" onClick={(e) => { e.preventDefault(); setRemember(!remember) }}>
+                <i className={`${remember ? 'fa-solid fa-circle-check text-blue-600' : 'fa-regular fa-circle text-zinc-400'} mr-3`}></i>
+                Remember me
+              </button>
+              <h5  className="text-base font-semibold text-blue-600">Forgot Password?</h5>
         </div>
-        <submit>Sign In</submit>
+        <input type="submit" className="h-14 w-full border border-zinc-300 rounded-xl bg-blue-600 text-white text-base font-bold mt-8 mb-8" value="Sign In"/>
       </form>
-      <p>Don't have an account?<button>Sign Up</button></p>
+        <p className="flex flex-row justify-center text-base font-medium">Don't have an account? <button className="font-bold ml-2">Sign Up</button></p>
+      </div>
     </div>
     
   )
--- tailwind.config.js
+++ tailwind.config.js
@@ -6,6 +6,9 @@ module.exports = {
   ],
   theme: {
     extend: {},
+    fontFamily: {
+      'rope':['Manrope', 'sans-serif']
+    }
   },
   plugins: [],
 }
